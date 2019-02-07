6830fd36c944f5676d5f21efa4328242c32c406a
Nahid Shiddo <43254494+nashiddo@users.noreply.github.com>
2019-02-07 17:27:27 +0000

    Final Changes

--- forEach.js
+++ forEach.js
@@ -1,15 +1,14 @@
-
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
- arr.forEach(function(name)){
-  if (name === "Waldo"){
-   found(i);
-  }
- }
+  arr.forEach(function(name){
+    if (name === "Waldo"){
+      found(arr.indexOf(name));
+    }
+  });
 }
-​
-function actionWhenFound(indexWaldo) {
- console.log("Found Waldo at index " + indexWaldo);
+
+function actionWhenFound(indexWaldo){
+  console.log("Found Waldo at index " + indexWaldo);
 }
-​
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound)
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
